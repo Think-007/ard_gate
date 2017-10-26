4e99aa747c7ff63995fede59a0d8aeb1cc551377
Think-007 <wylipengfei@163.com>
2017-10-26 21:27:06 +0800

    init

---
+++ src/main/java/com/thinker/gate/ArdGateApplication.java
@@ -0,0 +1,12 @@
+package com.thinker.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArdGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArdGateApplication.class, args);
+	}
+}
